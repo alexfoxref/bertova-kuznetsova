aa5a8a838e0294401202e1caecdba8c434000240
Alexander Demidenko <alexfoxref@gmail.com>
2019-08-08 14:01:58 +0500

    Поправил отображение кнопки в блоке Контакты, добавил js прокрутку по клику на ссылки в меню шапки и подвала

--- js/main.js
+++ js/main.js
@@ -1,17 +1,68 @@
-$(document).ready( () => {
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Функция смены класca className у элемента element
+    const toggleClass = (element, className) => {
+        if (element.classList.contains(className)) {
+            element.classList.remove(className)
+        } else {
+            element.classList.add(className)
+        }
+    };
+
+    // Выпадающие меню и кнопка выпадающего меню
+    const menuBtn = document.querySelector('.header__menu-btn'),
+        menu = document.querySelector('.header__menu');
+
+    menuBtn.addEventListener('click', () => {
+        toggleClass(menu, 'header__menu_active');
+        toggleClass(menuBtn, 'header__menu-btn_active')
+    });
+
+    // Отступы текста в блоге
+    const blogTitles = document.querySelectorAll('.blog__block-title'),
+        blogTexts = document.querySelectorAll('.blog__block-text'),
+        marginText = 20;
+    let titleMaxHeight = 0,
+        blogTitlesHeights = [];
+
+    blogTitles.forEach((title, index) => {
+        const titleHeight = title.getBoundingClientRect().height;
+        titleMaxHeight = (titleHeight > titleMaxHeight) ? titleHeight : titleMaxHeight;
+        blogTitlesHeights[index] = titleHeight;
+    });
+
+    blogTexts.forEach((text, index) => {
+        text.style.marginTop = `${titleMaxHeight + marginText - blogTitlesHeights[index]}px`
+    });
+
+    // Функция для прокрутки экрана к секции по клику на ссылки в меню menu
+    const scrollScreen = (menu) => {
+        const menuLinks = menu.querySelectorAll('a');
+
+        menu.addEventListener('click', event => {
+            event.preventDefault();
     
-    // Выпадающие меню
-    const menuBtn = $('.header__menu-btn'),
-        menu = $('.header__menu');
+            const target = event.target;
+    
+            menuLinks.forEach(link => {
+                if (target === link) {
+                    document.querySelector(link.getAttribute('href')).scrollIntoView({
+                            behavior: 'smooth',
+                            block: 'start'
+                        });
+                }
+            });
+        });
+    };
 
-    menuBtn.on('click', event => {
-        event.preventDefault();
-        menu.toggleClass('header__menu_active');
-    });
+    // Плавная прокрутка по клику на ссылки в меню в шапке и в подвале
+    const headerNavMenu = document.querySelector('.header-nav__menu'),
+        headerMenuList = document.querySelector('.header__menu-list'),
+        footerNavMenu = document.querySelector('.footer__nav-menu');
 
-    // Нажатие на кнопку выпадающего меню
-    menuBtn.on('click', () => {
-        menuBtn.toggleClass('header__menu-btn_active')
-    });
+    scrollScreen(headerNavMenu);
+    scrollScreen(headerMenuList);
+    scrollScreen(footerNavMenu);
+    
 
 });
