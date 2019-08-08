9feebecc8ac6d5152816ee62231ec0b434f819aa
Alexander Demidenko <alexfoxref@gmail.com>
2019-08-08 19:11:36 +0500

    Поправлены шрифты

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Функция смены класca className у элемента element
+const toggleClass = (element, className) => {
+    if (element.classList.contains(className)) {
+        element.classList.remove(className)
+    } else {
+        element.classList.add(className)
+    }
+};
 
-    // Функция смены класca className у элемента element
-    const toggleClass = (element, className) => {
-        if (element.classList.contains(className)) {
-            element.classList.remove(className)
-        } else {
-            element.classList.add(className)
-        }
-    };
-
-    // Выпадающие меню и кнопка выпадающего меню
-    const menuBtn = document.querySelector('.header__menu-btn'),
-        menu = document.querySelector('.header__menu');
-
-    menuBtn.addEventListener('click', () => {
-        toggleClass(menu, 'header__menu_active');
-        toggleClass(menuBtn, 'header__menu-btn_active')
-    });
-
-    // Отступы текста в блоге
+// Функция определения отступов текста от заголовков в блоке Блог
+const getBlogMargin = () => {
     const blogTitles = document.querySelectorAll('.blog__block-title'),
         blogTexts = document.querySelectorAll('.blog__block-text'),
         marginText = 20;
@@ -34,31 +24,48 @@ document.addEventListener('DOMContentLoaded', () => {
     blogTexts.forEach((text, index) => {
         text.style.marginTop = `${titleMaxHeight + marginText - blogTitlesHeights[index]}px`
     });
+};
 
-    // Функция плавной прокрутки к элементу element
-    const lightScroll = (element) => {
-        element.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+// Функция для прокрутки экрана к секции по клику на ссылки в меню menu
+const scrollScreen = (menu) => {
+    const menuLinks = menu.querySelectorAll('a');
+
+    menu.addEventListener('click', event => {
+        event.preventDefault();
+
+        const target = event.target;
+
+        menuLinks.forEach(link => {
+            if (target === link) {
+                lightScroll(document.querySelector(link.getAttribute('href')));
+            }
         });
-    };
+    });
+};
 
-    // Функция для прокрутки экрана к секции по клику на ссылки в меню menu
-    const scrollScreen = (menu) => {
-        const menuLinks = menu.querySelectorAll('a');
+// Функция плавной прокрутки к элементу element
+const lightScroll = (element) => {
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+};
 
-        menu.addEventListener('click', event => {
-            event.preventDefault();
-    
-            const target = event.target;
-    
-            menuLinks.forEach(link => {
-                if (target === link) {
-                    lightScroll(document.querySelector(link.getAttribute('href')));
-                }
-            });
-        });
-    };
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Выпадающие меню и кнопка выпадающего меню
+    const menuBtn = document.querySelector('.header__menu-btn'),
+        menu = document.querySelector('.header__menu');
+
+    menuBtn.addEventListener('click', () => {
+        toggleClass(menu, 'header__menu_active');
+        toggleClass(menuBtn, 'header__menu-btn_active')
+    });
+
+    // Отступы текста в блоге
+    getBlogMargin();
+
 
     // Плавная прокрутка по клику на ссылки в меню в шапке, подвале, на кнопку Заказа консультации на заглавном баннере
     const headerNavMenu = document.querySelector('.header-nav__menu'),
@@ -89,5 +96,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
     
-
 });
