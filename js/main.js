9fc53a3b02552bcb39fac3329011ef54fb4e41b5
Alexander Demidenko <alexfoxref@gmail.com>
2019-08-12 15:12:00 +0500

    Добавлена функция отправки формы без перезагрузки страницы с оповещением пользователя об отправке посредством модального окна

--- js/main.js
+++ js/main.js
@@ -40,8 +40,16 @@ const setBlogMargin = (margin) => {
     setMargin(blogTitles, blogTexts, margin);
 };
 
+// Функция плавной прокрутки к элементу element
+const lightScroll = (element) => {
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+};
+
 // Функция для прокрутки экрана к секции по клику на ссылки в меню menu
-const scrollScreen = (menu) => {
+const scrollScreen = menu => {
     const menuLinks = menu.querySelectorAll('a');
 
     menu.addEventListener('click', event => {
@@ -51,21 +59,172 @@ const scrollScreen = (menu) => {
 
         menuLinks.forEach(link => {
             if (target === link) {
+                // Закрытие выпадающего меню по клику на любую из его ссылок
+                
+                // Вариант последовательных действий: сначала закрывается меню (задержка обусловлена наличием transition: .1s), потом происходит переход
+                // const getMoveScreen = () => {
+                //     return new Promise(resolve => {
+                //         if (menu.classList.contains('header__menu-list')) {
+                //             const menu = document.querySelector('.header__menu'),
+                //                 menuBtn = document.querySelector('.header__menu-btn');
+        
+                //             if (menu.classList.contains('header__menu_active')) {
+                //                 toggleClass(menu, 'header__menu_active');
+                //                 toggleClass(menuBtn, 'header__menu-btn_active');
+                //             };
+                //         };
+                //         resolve();
+                //     });
+                // };
+
+                // const waitTimeout = (timeout) => {
+                //     return new Promise(resolve => {
+                //         setTimeout(resolve, timeout);
+                //     });
+                // };
+
+                // getMoveScreen()
+                //     .then(() => {
+                //         waitTimeout(1000)
+                //             .then(() => {
+                //                 lightScroll(document.querySelector(link.getAttribute('href')));
+                //             });  
+                //     });
+                    
+                if (menu.classList.contains('header__menu-list')) {
+                    const menu = document.querySelector('.header__menu'),
+                        menuBtn = document.querySelector('.header__menu-btn');
+
+                    if (menu.classList.contains('header__menu_active')) {
+                        toggleClass(menu, 'header__menu_active');
+                        toggleClass(menuBtn, 'header__menu-btn_active');
+                    };
+                };
+                
                 lightScroll(document.querySelector(link.getAttribute('href')));
-            }
+            };
         });
     });
 };
 
-// Функция плавной прокрутки к элементу element
-const lightScroll = (element) => {
-    element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
+// Сервис
+
+// Адрес api (json-server)
+const _apiBase = 'http://localhost:3000';
+// Контент модального окна в зависимости от состояния выполнения запроса
+const statusMessages = {
+    loading: `
+        <div class="spinner">
+            <div class="lds-spin"><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div></div>
+        </div>
+    `,
+    success: `
+        <h4 class="modal__title">Спасибо</h4>
+        <p class="modal__text">Ваша заявка отправлена</p>
+        <button class="btn btn__primary modal__btn">Закрыть</button>
+    `,
+    error: `
+        <img class="modal__img" src="./img/error.png" alt="error">
+        <p class="modal__text">Что-то пошло не так. Повторите попытку позже</p>
+        <button class="btn btn__primary modal__btn">Закрыть</button>
+    `
+};
+
+// Функция отображения результата отправки формы
+const getStatusForm = (modal, modalContent, content) => {
+    modal.classList.add('modal__active');
+    modalContent.innerHTML = '';
+    document.body.style.overflow = 'hidden';
+
+    const status = document.createElement('div');
+
+    status.innerHTML = `
+        ${content}
+    `;
+    status.classList.add('modal__message');
+    modalContent.appendChild(status);
+};
+
+// Функция закрытия модального окна
+const closeModal = () => {
+    const modalBtn = document.querySelector('.modal__btn');
+
+    modalBtn.addEventListener('click', () => {
+        toggleClass(modal, 'modal__active');
+        document.body.style.overflow = '';
+    })
+};
+
+// Отображение результатов отправки формы
+const modal = document.querySelector('.modal'),
+    modalContent = document.querySelector('.modal__content');
+
+// Функция загрузки
+const getLoading = () => getStatusForm(modal, modalContent, statusMessages.loading);
+
+// Функция успешной отправки формы
+const getSuccess = () => {
+    getStatusForm(modal, modalContent, statusMessages.success);
+    closeModal();
+};
+
+// Функция ошибки при отправке формы
+const getError = () => {
+    getStatusForm(modal, modalContent, statusMessages.error)
+    closeModal();
+};
+
+// Функция отправки POST запроса
+const postData = async (url, data) => {
+    getLoading();
+
+    try {
+        const res = await fetch(`${_apiBase + url}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (res.ok) {
+            getSuccess();
+        }
+    } catch {
+        getError();
+        throw new Error(`Could not fetch ${_apiBase + url}`);
+    }
+};
+
+// Функция очистки полей для ввода при отправке формы
+const clearInputs = (form) => {
+    let inputs = form.querySelectorAll('input');
+
+    inputs.forEach(input => {
+        input.value = '';
+    })
+};
+
+// Функция отправки формы
+const sendForm = (form, url) => {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const formData = new FormData(form),
+            obj = {};
+
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        });
+
+        postData(url, obj);
+
+        clearInputs(form);
     });
 };
 
 
+// Работа скриптов при загрузке элементов страницы
 document.addEventListener('DOMContentLoaded', () => {
 
     // Выпадающие меню и кнопка выпадающего меню
@@ -109,6 +268,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });   
     
+    // Отправка форм из блоков Консультация и Контакты на url
+    const formConsult = document.querySelector('.consult__form'),
+        formContacts = document.querySelector('.contacts__form'),
+        url = '/contacts';
+
+    sendForm(formConsult, url);
+    sendForm(formContacts, url);
+
     // Действия при изменении размеров экрана
     window.addEventListener('resize', () => {
         // Удаление активного класса у кнопки выпадающего меню и меню
@@ -116,8 +283,8 @@ document.addEventListener('DOMContentLoaded', () => {
             menu = document.querySelector('.header__menu');
         
         if (menu.classList.contains('header__menu_active')) {
-            menu.classList.remove('header__menu_active');
-            menuBtn.classList.remove('header__menu-btn_active');
+            toggleClass(menu, 'header__menu_active');
+            toggleClass(menuBtn, 'header__menu-btn_active');
         };
     
         // Отступы текста от заголовка в блоге
@@ -125,4 +292,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
-
